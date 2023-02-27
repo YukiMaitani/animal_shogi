2269af2d5093dd5982bbdbd05e08788fa43a2836
YukiMaitani <fightzoi1997@gmail.com>
2023-02-27 23:46:31 +0900

    盤の枠と色を設定

--- lib/foundation/colors.dart
+++ lib/foundation/colors.dart
@@ -6,3 +6,6 @@ const Color paletteButtonOnColor = Color(0xffa5e69b);
 Color paletteSliderBackgroundColor = const Color(0xffbe9f77).withOpacity(0.95);
 const Color paletteSliderColor = Color(0xffb0daa8);
 const Color drawPageBodyColor = Color(0xFFfff2e0);
+const Color boardBodyColor = Color(0xFFfefce3);
+const Color boardFrameBorderColor = Color(0xFFffeddc);
+const Color gamePageBodyColor = Color(0xFFfef8f0);
--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,17 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../foundation/colors.dart';
+
 class GamePage extends HookConsumerWidget {
   const GamePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-   return Scaffold(
-     body: _buildBody(),
-   );
+    return Scaffold(
+      body: _buildBody(),
+      backgroundColor: gamePageBodyColor,
+    );
   }
 
   Widget _buildBody() {
-    return Column();
+    return Column(
+      children: [
+        const Spacer(),
+        _buildBoard(),
+        const Spacer(),
+      ],
+    );
+  }
+
+  Widget _buildBoard() {
+    return HookConsumer(builder: (context, ref, child) {
+      final baseWidth = MediaQuery.of(context).size.width * 0.8;
+      final baseHeight = baseWidth / 3 * 4;
+      return Center(
+        child: Container(
+          width: baseWidth,
+          height: baseHeight,
+          decoration: BoxDecoration(
+              color: boardBodyColor,
+              border: Border.all(
+                color: boardFrameBorderColor,
+                width: 4,
+              )),
+        ),
+      );
+    });
   }
 }
