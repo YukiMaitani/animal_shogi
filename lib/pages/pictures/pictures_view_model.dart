6e947ae90867012abaa84e803073391009d8149d
YukiMaitani <fightzoi1997@gmail.com>
2023-03-07 08:39:17 +0900

    ファイル削除

--- lib/pages/pictures/pictures_view_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final picturesProvider = ChangeNotifierProvider.autoDispose((ref) => PicturesViewModel());
-
-class PicturesViewModel extends ChangeNotifier {
-
-}
