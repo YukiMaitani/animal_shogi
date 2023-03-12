3f95a0148097255bcc3a2dc3c83337795e9b2feb
YukiMaitani <fightzoi1997@gmail.com>
2023-03-12 22:27:01 +0900

    ゲームのマスが表示されないバグを修正

--- lib/foundation/colors.dart
+++ lib/foundation/colors.dart
@@ -8,6 +8,6 @@ const Color paletteSliderColor = Color(0xffb0daa8);
 const Color drawPageBodyColor = Color(0xFFfff2e0);
 const Color boardBodyColor = Color(0xFFfefce3);
 const Color boardFrameBorderColor = Color(0xFFffeddc);
-const Color gamePageBodyColor = Color(0xFFfef8f0);
+const Color gamePageBodyColor = Color(0xFFfdefe6);
 const Color paletteBackForwardButtonOffColor = Color(0xFF857f78);
 const Color paletteBackForwardButtonOnColor = Color(0xFF4f4538);
--- lib/pages/game/board_custom_painter.dart
+++ lib/pages/game/board_custom_painter.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 
 import '../../foundation/colors.dart';
 
-
 class BoardCustomPainter extends CustomPainter {
   BoardCustomPainter();
 
   @override
   void paint(Canvas canvas, Size size) {
-    final pathPaint = Paint()..color = boardFrameBorderColor..strokeWidth = 1;
-    final baseWidth = size.width / 8;
+    final pathPaint = Paint()
+      ..color = boardFrameBorderColor
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+    final baseWidth = size.width / 3;
     for (var i = 0; i < 5; i++) {
       final path = Path()
         ..moveTo(baseWidth * i, 0)
@@ -18,7 +20,7 @@ class BoardCustomPainter extends CustomPainter {
     }
     for (var i = 0; i < 4; i++) {
       final path = Path()
-        ..moveTo(0, baseWidth * 1)
+        ..moveTo(0, baseWidth * i)
         ..lineTo(baseWidth * 3, baseWidth * i);
       canvas.drawPath(path, pathPaint);
     }
